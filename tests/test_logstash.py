6ac8c15bc507075545b63fc9271ea2ffd8c22585
Royce Remer <royceremer@gmail.com>
2015-03-01 20:31:29 -0800

    add tcp transport/decoding tests

--- tests/test_logstash.py
+++ tests/test_logstash.py
@@ -1,6 +1,16 @@
+import json
+import logging
+from multiprocessing import Process, Queue
 import sys
 import unittest
 import logstash
+
+# work around renamed python3 socketserver module
+if sys.version_info[0] > 2:
+    import socketserver
+    SocketServer = socketserver
+else:
+    import SocketServer
 
 
 class FormatterTestCase(unittest.TestCase):
@@ -52,19 +62,80 @@ class FormatterTestCase(unittest.TestCase):
                 % (version, self.current_version,
                    repr(invoc_args), error_msg)))
 
+    def test_formatter_sanitization(self):
+        """Assert proper sanitization of characters in json namespace."""
+        pass
+
 
 class HandlerAmqpTestCase(unittest.TestCase):
     pass
 
 
 class HandlerTcpTestCase(unittest.TestCase):
-    pass
+
+    def setUp(self):
+        # logstash event schema versions
+        self.logstash_versions = StoredRequestHandler.version_dict
+
+        self.server = SocketServer.TCPServer(('localhost', 0), StoredRequestHandler)
+        hostport_tuple = self.server.socket.getsockname()
+        self.host = hostport_tuple[0]
+        self.port = hostport_tuple[1]
+        # thread the server non-blocking for our tests
+        self.server_thread = Process(target=self.server.serve_forever)
+        self.server_thread.daemon = True
+        self.server_thread.start()
+
+    def tearDown(self):
+        self.server_thread.terminate()
+
+    def test_base_tcp_decoding(self):
+        """Assert json decoding of TCP message."""
+        test_str = u'test-runner: simple message'
+        for version in list(self.logstash_versions.keys()):
+           test_logger = logging.getLogger('python-logstash-logger')
+           test_logger.setLevel(logging.INFO)
+           test_logger.addHandler(logstash.TCPLogstashHandler(self.host,
+                                                              self.port,
+                                                              version=version))
+           test_logger.error(test_str)
+           recv_str = self.server.RequestHandlerClass.recv_queue.get()
+           try:
+               recv_dict = json.loads(recv_str)
+           except ValueError as e:
+               self.fail("String '%s' could not be parsed as json." %recv_str)
+           err_msg=("Parsed json: '%s' did not have matching message '%s'."
+                    % (recv_str, test_str))
+           version_keys = self.logstash_versions[version]
+           try:
+               recv_dict[version_keys['msg_str']]
+           except KeyError:
+               self.fail("String '%s' could not be parsed as json." %recv_str)
+           self.assertEqual(recv_dict[version_keys['msg_str']],
+                            test_str, msg=err_msg)
 
 
 class HandlerUdpTestCase(unittest.TestCase):
     pass
 
 
+class StoredRequestHandler(SocketServer.BaseRequestHandler):
+    """Store requests from any protocol server."""
+
+    recv_queue = Queue()
+
+    # this data should be inherited from LogstashFormatterVersion{0,1}
+    # but until then, and since this class is used by all test cases...
+    version_dict = {0:{'msg_str':'@message', 'time_str':'@timestamp'},
+                    1:{'msg_str':'message', 'time_str':'@timestamp'}}
+
+    def handle(self):
+        raw_str = self.request.recv(1024)
+        coerced_str = raw_str.strip().decode('utf-8', 'ignore')
+        self.recv_queue.put(coerced_str)
+
+
+# Add python3 assertion apis to python 2.6 env
 if sys.version_info[0] == 2 and sys.version_info[1] < 7:
     def assertIsInstance(self, obj, cls, msg=None):
         """Same as self.assertTrue(isinstance(obj, cls)), with a nicer
